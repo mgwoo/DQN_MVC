4e7b9a5c107d601729ad58a67e9bf9a9fbbf2e51
mgwoo <mwoo@eng.ucsd.edu>
2020-05-12 19:16:48 -0700

    backup

--- Q_function_graph_model2.py
+++ Q_function_graph_model2.py
@@ -51,14 +51,16 @@ def Q_func(x, adj, w, p, T, initialization_stddev,
                     for i in range(pre_pooling_mlp_layers):
                         mu = tf.nn.relu(tf.einsum('kl,ivk->ivl', Ws_pre_pooling[i], mu) + bs_pre_pooling[i])
 
-                mu_part2 = tf.einsum('kl,ivk->ivl', theta2, tf.einsum('ivu,iuk->ivk', adj, mu))
+                # tf.sprase_dense_matmul(adj, mu);
+
+                mu_part2 = tf.einsum('kl,ivk->ivl', theta2, tf.einsum('ivu,iuk->ivk', tf.sparse.to_dense(adj), mu))
                 # Add some non linear transformations of the pooled neighbors' embeddings
                 with tf.variable_scope('post_pooling_MLP', reuse=False):
                     for i in range(post_pooling_mlp_layers):
                         mu_part2 = tf.nn.relu(tf.einsum('kl,ivk->ivl', Ws_post_pooling[i], mu_part2) + bs_post_pooling[i])
 
             # Third part of mu
-            mu_part3_0 = tf.einsum('ikvu->ikv', tf.nn.relu(tf.einsum('k,ivu->ikvu', theta4, w)))
+            mu_part3_0 = tf.einsum('ikvu->ikv', tf.nn.relu(tf.einsum('k,ivu->ikvu', theta4, tf.sparse.to_dense(w))))
             mu_part3_1 = tf.einsum('kl,ilv->ivk', theta3, mu_part3_0)
 
             # All all of the parts of mu and apply ReLui
@@ -68,7 +70,7 @@ def Q_func(x, adj, w, p, T, initialization_stddev,
                 mu = tf.nn.relu(tf.add(mu_part1, mu_part3_1, name='mu_' + str(t)))
 
         # Define the Qs
-        Q_part1 = tf.einsum('kl,ivk->ivl', theta6, tf.einsum('ivu,iuk->ivk', adj, mu))
+        Q_part1 = tf.einsum('kl,ivk->ivl', theta6, tf.einsum('ivu,iuk->ivk', tf.sparse.to_dense(adj), mu))
         Q_part2 = tf.einsum('kl,ivk->ivl', theta7, mu)
         return tf.identity(tf.einsum('k,ivk->iv', theta5,
                                      tf.nn.relu(tf.concat([Q_part1, Q_part2], axis=2))),
